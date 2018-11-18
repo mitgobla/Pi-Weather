96264440ba1d39b00633f5718715a92a17771aeb
mitgobla <ben-dodd@outlook.com>
2018-11-18 11:42:47 +0000

    Added Temp and Humidity

--- pi_weather.py
+++ pi_weather.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 from time import sleep
 from papirus import PapirusComposite
@@ -5,10 +6,11 @@ from weather import Weather, Unit
 
 directory_path = os.path.dirname(os.path.realpath(__file__))
 
-weather = Weather(unit=Unit.CELSIUS)
 ink_display = PapirusComposite(False)
 first_iteration = True
+
 while True:
+    weather = Weather(unit=Unit.CELSIUS)
     lookup = weather.lookup_by_location('llandysul')
 
     sunrise = lookup.astronomy.sunrise
@@ -19,15 +21,17 @@ while True:
     wind_speed = lookup.wind.speed+lookup.units.speed
     wind_direction = lookup.wind.direction
 
-    humidity = str(lookup.atmosphere.humidity)+"%"
-    temperature = lookup.condition.temp+lookup.units.temperature
+    humidity = str(lookup.atmosphere.humidity)+"%"+" rH"
+    temperature = lookup.condition.temp+"°"+lookup.units.temperature
     weather_type = lookup.condition.text
     weather_code = lookup.condition.code
     if first_iteration:
-        ink_display.AddImg(os.path.join(directory_path, 'images', 'weather', str(weather_code)+'.png'), 0, 0, (96, 96), Id = "WeatherIcon")
-        ink_display.AddText(weather_type, 96, 0, size=10, Id="WeatherText")
+        ink_display.AddImg(os.path.join(directory_path, 'images', 'weather', str(weather_code)+'.png'), 0, 0, (48, 48), Id = "WeatherIcon")
+        ink_display.AddText(weather_type, 48, 0, size=16, Id="WeatherText")
+        ink_display.AddText(temperature+"    "+humidity, 48, 16, size=16, Id="TempHumdText")
     else:
         ink_display.UpdateImg("WeatherIcon", os.path.join(directory_path, 'images', 'weather', str(weather_code)+'.png'))
-        ink_display.UpdateText("WeatherText", lookup.condition.text)
+        ink_display.UpdateText("WeatherText", weather_type)
+        ink_display.UpdateText("TempHumdText", temperature+"    "+humidity)
     ink_display.WriteAll()
     sleep(60)
